d284f6fd1a166ba41129178d5c84fd51e7e8043e
keh0720 <keh0720@gmail.com>
2020-08-17 23:36:22 +0900

    Add position info

--- GameOne.py
+++ GameOne.py
@@ -1,21 +1,19 @@
 import requests
-import GameOneCrawler.Players
 from bs4 import BeautifulSoup
-
 from GameOneCrawler.Players import Players
 
 team_url = "http://www.gameone.kr/club/info/player?club_idx=13457"
 player_url = "http://www.gameone.kr/locker/record/sum"
 startYear = 2018
-endYear = 2019
+endYear = 2018
 
 players = Players.get_players(team_url)
 group_code = '3D6307527DE36B0FF0992CA9C8F9F3B3'
 total_records = dict()
 for year in range(startYear, endYear + 1):
     records = dict()
-    for name, group_code in players.items():
-        url = player_url + "?group_code=" + group_code + "&season=" + str(year)
+    for name, info in players.items():
+        url = player_url + "?group_code=" + info['group_code'] + "&season=" + str(year)
         soup = BeautifulSoup(requests.get(url).text, 'html.parser')
 
         titles_html = soup.select(".score_record > ul li span.title")
@@ -23,9 +21,7 @@ for year in range(startYear, endYear + 1):
         titles = [title.text for title in titles_html]
         scores = [score.text for score in scores_html]
         len(titles_html)
-        record = dict(zip(titles[:28], scores[:28])) # only hitter record
-
-        print(name + ":" + record)
+        record = dict(zip(titles[:28], scores[:28]))  # only hitter record
         records[name] = record
     total_records[year] = records
 
--- Players/Players.py
+++ Players/Players.py
@@ -7,10 +7,18 @@ def get_players(url):
     player_group = soup.select(".player_list")
     group_code_html = soup.find_all('input', {'name': 'group_code'})
     name_html = soup.select("div.scon_players._helix > ul > li > dl > form > dt")
+    position_html = soup.select("div.scon_players._helix > ul > li > dl > form > dd:nth-child(4)")
+    # sContent > div.s_content > div.scon_players._helix > ul > li:nth-child(21) > dl > form > dd:nth-child(4)
     group_codes = [group_code.attrs.get('value') for group_code in group_code_html]
     names = [name.text for name in name_html]
-
-    return dict(zip(names, group_codes))
+    positions = [str(position.text).split("|")[0].rstrip() for position in position_html]
+    i = 0
+    players = dict()
+    for name in names:
+        info = dict(group_code=group_codes[i], position=positions[i])
+        players[name] = info
+        i += 1
+    return players
 
 
 if __name__ == "__main__":
